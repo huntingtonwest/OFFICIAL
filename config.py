691a5d7980b8b8afb510fcb3d66e762315d87347
swagufied <cooljoshua11@gmail.com>
2018-04-01 21:43:54 -0400

    prod test

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or '4719fa05-d059-4ebc-9dc4-d447c2207053'
-# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-SQLALCHEMY_DATABASE_URI =  'postgres://postgres:2dover3d@localhost/hwp'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# SQLALCHEMY_DATABASE_URI =  'postgres://postgres:2dover3d@localhost/hwp'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MAIL_PORT = 465
@@ -17,6 +17,3 @@ MAIL_SERVER = 'smtp.gmail.com'
 serializer_key  = '27f06ca6-b864-4ff9-895b-42abd7ca8a22'
 
 PROPERTIES_PER_PAGE = 20
-
-
-

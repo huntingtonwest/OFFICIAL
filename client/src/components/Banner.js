0f09c59ce0f9ea369720b9901491e22a32c6c95b
Elif Aydin <eaydin@usc.edu>
2018-04-09 23:52:32 -0700

    new title font

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import LazyLoad from 'react-lazy-load';
 
 class Banner extends React.Component {
 
@@ -10,20 +9,16 @@ class Banner extends React.Component {
       backgroundImage: "url(" + bg + ")",
       backgroundRepeat:'no-repeat',
       backgroundPosition: 'center center',
-      backgroundSize:'cover',
-      height: 550,
-      marginTop: -100
+      backgroundSize:'cover'
     };
 
     return (
-      <LazyLoad height={550} offsetVertical={1000}>
       <div className="text-center search-banner" style={divStyle}>
         <div className="search-inner">
           <h1 className="title-banner title-lower-banner">{this.props.title}</h1>
           <br />
         </div>
       </div>
-      </LazyLoad>
     );
   }
 }
